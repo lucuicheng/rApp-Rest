7348e49fd6c7be027534d8fc82652a5b9ad87e02
lucuicheng <cch.lu@canyangroup.com>
2019-10-16 16:11:10 +0800

    Description:[Feature:000] 添加 mongoose 支持 Module:Rest Basic Reviewer:@lucuicheng

--- app/app.js
+++ app/app.js
@@ -1,16 +1,33 @@
 const http = require('http'),
-      Koa = require('koa'),
-      config = require('config'),
-      err = require('./helpers/error'),
-     {routes, allowedMethods}  = require('./routes'),
-      app = new Koa();
+    Koa = require('koa'),
+    config = require('config'),
+    err = require('./helpers/error'),
+    mongoose = require("mongoose"),//使用 mongoose 操作数据库
+    {routes, allowedMethods} = require('./routes'),
+    app = new Koa();
 
 app.use(err);
 app.use(routes());
 app.use(allowedMethods());
 
+const connectMongoDB = () => {
+    console.log('connecting mongodb...');
+    const dbUrl = process.env.MONGO_URL || 'mongodb://192.168.0.100:27017/TA';//数据库地址
+    mongoose.connect(dbUrl, {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true});//尝试连接数据库
+    const db = mongoose.connection;
+    db.on('open', function (res) {
+        console.log(' mongodb connected successful!');
+    });
+    db.once('error', function (error) {
+        console.log(' mongodb database connect failed ' + error);
+    });
+
+    return db;
+};
+
 const server = http.createServer(app.callback()).listen(config.server.port, function () {
     console.log('%s listening at port %d', config.app.name, config.server.port);
+    connectMongoDB();
 });
 
 module.exports = {
